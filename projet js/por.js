e62d311a63cf006098d77e717a7e3d0991da8040
Tiguilionne <48598647+Tiguilionne@users.noreply.github.com>
2019-03-28 18:16:34 +0000

    init
    init

---
+++ projet js/por.js
@@ -0,0 +1,9 @@
+
+
+	var x = document.getElementById("garb");
+    	var y = document.getElementById('garb');
+    	function vien(){
+          x.src="css/imges/garbb.jpg";}
+          function pat(){
+          	y.src="css/imges/garba.jpg";}
+         
